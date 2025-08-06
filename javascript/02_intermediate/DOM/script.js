f86443cd2dfca15df90c707401b092967609e9d0
Muhammad Zuhaib <mzuhaibmalik786@gmail.com>
2025-08-06 14:22:42 +0500

    learning javascript

--- javascript/02_intermediate/DOM/script.js
+++ javascript/02_intermediate/DOM/script.js
@@ -1,3 +1,38 @@
-console.log(window);
-console.log(document.getElementsByTagName(h1));
+// 1 => Selection of Element 
+// with queryselctor
 
+
+var selected = document.querySelector("h1");
+// changing inner html
+console.log(selected);
+
+// changing css
+selected.innerHTML = "this is changed"
+selected.style.color = "blue"
+
+// event listener
+
+selected.addEventListener("click", function(){
+    selected.innerHTML = "this is changed due to click";
+    selected.style.color = "Red";
+});
+
+
+// implemanting toggle button 
+
+
+var bbul = document.querySelector("#bulb");
+var sbulb = document.querySelector("#hh");
+var flag = null;
+sbulb.addEventListener("click", function(){
+    if(flag == 0){
+        bbul.style.backgroundColor = "black";
+        sbulb.innerHTML = "ON"
+        flag = 1;
+    }else{
+        bbul.style.backgroundColor = "yellow";
+        sbulb.innerHTML = "OFF"
+        flag = 0;
+    }
+
+})
