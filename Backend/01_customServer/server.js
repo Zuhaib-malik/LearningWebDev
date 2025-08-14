9ef55623ff8d7bcd440e4945d8b14d6c7c0bc7a2
Muhammad Zuhaib <mzuhaibmalik786@gmail.com>
2025-08-15 00:37:10 +0500

    updated server.js

--- Backend/01_customServer/server.js
+++ Backend/01_customServer/server.js
@@ -6,9 +6,22 @@ const path = require('path');
 const port = 4000;
 
 
-const server = http.createServer();
+const server = http.createServer((req , res) = () => {
+    const pathname = path.join(__dirname, req.url === '/' ? 'index.html': 'req.url' )
+    const extname = String(path.extname(pathname)).toLowerCase();
+
+    const mimeType = {
+        '.html' : 'text/html',
+        '.css' : 'text/css',
+        '.js' : 'text/javaScript'
+    }
+   const contentType =  mimeType[extname] || 'application/octet-stream'
+});
 
 server.listen(port, ()=>{
     console.log(`server is listening on ${port} port number`);
     
 })
+
+// 26 min
+
