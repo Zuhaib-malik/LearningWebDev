33d37316b557bbb622c52203eee9f163fd1e3d89
Muhammad Zuhaib <mzuhaibmalik786@gmail.com>
2025-08-16 23:24:07 +0500

    build a server having two routes

--- BackendSeries/01_web_server/server_node.js
+++ BackendSeries/01_web_server/server_node.js
@@ -4,9 +4,19 @@ const hostname = '127.0.0.1'; // Localhost
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n'); // Send response
+    if(req.url === '/'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World\n'); // Send response
+    }else if(req.url === '/sec'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('theis is second response\n'); // Send response
+    }else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404 not Found\n'); // Send response
+    }
 });
 
 server.listen(port, hostname, () => {
